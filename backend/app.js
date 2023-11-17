2c798cf35794a39f13e98fbd90a62fb51b96cfa4
syh <dbsgur6896@gmail.com>
2023-11-17 19:40:30 +0900

    ✨ swagger docs 추가

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,11 @@ const quizRouter = require('./routes/quiz');
 const videoRouter = require('./routes/video');
 const { sequelize } = require('./models');
 
+//swagger docs
+var bodyParser = require("body-parser");
+var swaggerJsdoc = require("swagger-jsdoc");
+var swaggerUi = require("swagger-ui-express");
+
 dotenv.config();
 
 const app = express();
@@ -24,8 +29,50 @@ const cors = require('cors');
 app.use(cors())
 app.use(express.json());
 
+//swagger
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Quiz App API",
+        version: "0.1.0",
+        description:
+          "This is a simple CRUD API application made with Express and documented with Swagger",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+          name: "test",
+          url: "https://test.com",
+          email: "info@email.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:4000",
+        },
+      ],
+    },
+    apis: [
+        "./swagger/user.js",
+    ],
+  };
+  
+const specs = swaggerJsdoc(options);
+  app.use("/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+  );
+
 app.use('/api/users', userRouter);
 app.use('/api/quiz', quizRouter);
 app.use('/api/videos', videoRouter);
+
+//swagger
+app.use("/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
