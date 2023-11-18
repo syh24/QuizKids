d232f652aceb241e14c0da71ac5def15ed9a0fa3
Mengou <gjsehdrh@gmail.com>
2023-11-18 16:28:52 +0900

    add profile

--- frontend/src/views/Main.js
+++ frontend/src/views/Main.js
@@ -1,28 +1,80 @@
+import React, {useState, useRef, useEffect} from 'react';
+
 import TabLayout, {Tab} from '@enact/sandstone/TabLayout';
 import {Header, Panel} from '@enact/sandstone/Panels';
-import {VirtualList, VirtualGridList} from '@enact/sandstone/VirtualList';
-import Button from '@enact/sandstone/Button';
-import Item from '@enact/sandstone/Item';
-import Icon from '@enact/sandstone/Icon';
-import ri from '@enact/ui/resolution';
-
 import $L from '@enact/i18n/$L';
 import Home from './Home';
 import Account from './Account';
-import { useState, useEffect } from 'react';
+import Button from '@enact/sandstone/Button';
+import Item from '@enact/sandstone/Item';
+import Icon from '@enact/sandstone/Icon';
+import Profile from './Profile';
+import MediaOverlay from '@enact/sandstone/MediaOverlay';
+import ImageItem from '@enact/ui/ImageItem';
+import { VirtualGridList } from '@enact/sandstone/VirtualList';
+import css from './Profile.module.less';
 
-const Main = (props) => {
+// hover 상태에 따라 video를 제어하는 Component
+const HoverVideoOverlay = ({src, ...rest}) => {
+	const [isHovering, setIsHovering] = useState(false);
+	const videoRef = useRef(null);
+
+	const handleMouseEnter = () => {
+		setIsHovering(true);
+		if (videoRef.current) {
+			videoRef.current.play();
+		}
+	};
+
+	const handleMouseLeave = () => {
+		setIsHovering(false);
+		if (videoRef.current) {
+			videoRef.current.pause();
+		}
+	};
+
 	return (
-		<Panel {...props}>
-			<Header title={$L('Enact Template')} />
-				<TabLayout>
-					<Tab title={$L('Home')} icon = "home">
-						<Home />
-					</Tab>
-					<Tab title={$L('Account')} icon = "profile">
-						<Account />
-					</Tab>
-				</TabLayout>
+		<div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+			<MediaOverlay {...rest}>
+				<video ref={videoRef} muted loop>
+					<source src={src} type="video/mp4" />
+				</video>
+			</MediaOverlay>
+		</div>
+	);
+};
+
+const Main = props => {
+	const onClick = () => {
+		console.log("hello");
+	};
+
+	return (
+
+		<Panel>	
+			<Header title="Quiz App" closeButtonBackgroundOpacity="transparent">
+				<Button size="small" icon="profile" slot="slotAfter" onClick={onClick}/>
+			</Header>
+			<TabLayout
+				onSelect={function noRefCheck() {}}
+				onTabAnimationEnd={function noRefCheck() {}}
+				orientation="vertical"
+				tabSize={null}
+			>
+				<Tab title={$L('Home')} icon="home">
+					<Home />
+				</Tab>
+				<Tab icon="gear" title="Settings">
+					<Button icon="demosync">Button 1</Button>
+					<Button icon="demosync">Button 2</Button>
+					<Button icon="demosync">Button 3</Button>
+					<Button icon="demosync">Button 4</Button>
+					<Button icon="demosync">Button 5</Button>
+				</Tab>
+				<Tab icon="list" title="History">
+					<Item slotBefore={<Icon>playcircle</Icon>}>Single Item</Item>
+				</Tab>
+			</TabLayout>
 		</Panel>
 	);
 };
