def63bef39d30c82dbb5fc51823e094d1f65555a
Mengou <gjsehdrh@gmail.com>
2023-11-20 02:39:28 +0900

    possible full screen video when click video & add plus button when click pause

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -10,28 +10,50 @@ import Media from './Media';
 import ImageItem from '@enact/sandstone/ImageItem';
 import Scroller from '@enact/ui/Scroller';
 import MediaOverlay from '@enact/sandstone/MediaOverlay';
+import { useState } from 'react';
+import Detail from './Detail';
 
 const Home = () => {
+	const [currentVideoSrc, setCurrentVideoSrc] = useState("");
+	const [videoSources, setVideoSources] = useState([
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4",
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4"
+	])
+	
+	const onClickVideo = (event) => {
+		currentVideoSrc === "" ? setCurrentVideoSrc(event.target.currentSrc) : setCurrentVideoSrc("");
+	};
+
 	return (
 		<>
-			<BodyText>{$L('Video List')}</BodyText>	
-			<Scroller direction='vertical'>
-				<div style={{display: 'flex', flexWrap: 'wrap', }}>
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"/>
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" />
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4" />
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" />
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4" />
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4" />
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4" />
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4" />
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4" />
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4" />
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4" />
-					<Media src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4" />	
-				</div>
-			</Scroller>
+		{currentVideoSrc === "" ? 
+			(
+				<>
+					<BodyText>{$L('Video List')}</BodyText>
+					<Scroller direction='vertical'>
+					<div style={{ display: 'flex', flexWrap: 'wrap' }}>
+						{videoSources.map((src, index) => (
+						<Media onClick={onClickVideo} idx={index} src={src} key={index} />
+						))}
+					</div>
+					</Scroller>
+				</>
+			) : 
+			(
+				<Detail src={currentVideoSrc} onBack={onClickVideo}/>
+			)
+		}
 		</>
 	);
 };
