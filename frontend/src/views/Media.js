77dde859d399b3f5224aa2537212e62bfd2f0ac9
Mengou <gjsehdrh@gmail.com>
2023-11-20 02:35:34 +0900

    add onClick eventListener for detailed video

--- frontend/src/views/Media.js
+++ frontend/src/views/Media.js
@@ -1,19 +1,30 @@
+import React, {useState} from 'react';
 import MediaOverlay from '@enact/sandstone/MediaOverlay';
-// rendering Media Overlay
 
-const Media = (prop) => {
+const Media = ({onClick, idx, src}) => {
+	const [isHovering, setIsHovering] = useState(false);
 	return (
-		<div>
+		<div
+			onMouseEnter={() => setIsHovering(true)}
+			onMouseLeave={() => setIsHovering(false)}
+			className="rounded-2xl overflow-hidden"
+			onClick = {onClick} // added for detailed video
+			id={idx}
+		>
 			<MediaOverlay
-                caption="DTV 7-1"
-                marqueeOn="focus"
-                muted
-                progress={0.5}
-                subtitle="07:00 AM - 08:00 AM"
-                textAlign="end"
-                title="Program Name"
-            >
-				<source src={prop.src} />
+				marqueeOn="focus"
+				muted={!isHovering}
+				noAutoPlay={!isHovering}
+				progress={0.5}
+				textAlign="end"
+				title="Program Name"
+				imageOverlay={
+					isHovering ? null : 'https://via.placeholder.com/1600x900'
+				} // Show image when not hovering
+				className="rounded-2xl overflow-hidden"
+			>
+				{/* Only set the source if we are hovering */}
+				{isHovering && <source src={src} />}
 			</MediaOverlay>
 		</div>
 	);
