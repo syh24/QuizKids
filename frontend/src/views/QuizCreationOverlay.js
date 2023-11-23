556673d1e85280d1ea973b55086982630413e79f
강상원 <kevink1113@u.sogang.ac.kr>
2023-11-23 20:04:26 +0900

    ✨ Update QuizCreation to handle multiple steps

--- frontend/src/views/QuizCreationOverlay.js
+++ frontend/src/views/QuizCreationOverlay.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {WizardPanels, Slottable, Panel} from '@enact/sandstone/WizardPanels';
 import Button from '@enact/sandstone/Button';
 import Input from '@enact/sandstone/Input';
 import Checkbox from '@enact/sandstone/Checkbox';
@@ -6,38 +7,63 @@ import Popup from '@enact/sandstone/Popup';
 import Scroller from '@enact/sandstone/Scroller';
 
 const QuizCreationOverlay = ({onClose, timestamp, src}) => {
+	const [current, setCurrent] = useState(0);
+	const [questionType, setQuestionType] = useState('');
 	const [question, setQuestion] = useState('');
-	const [options, setOptions] = useState(['', '']);
+	const [options, setOptions] = useState(['', '', '', '']);
 	const [correctAnswer, setCorrectAnswer] = useState(null);
 
-	const handleAddOption = () => {
-		setOptions([...options, '']);
-	};
-
-	const handleOptionChange = (index, value) => {
-		const newOptions = [...options];
-		newOptions[index] = value;
-		setOptions(newOptions);
-	};
-
-	const handleDeleteOption = index => {
-		const newOptions = options.filter(
-			(_, optionIndex) => optionIndex !== index
-		);
-		setOptions(newOptions);
-		if (correctAnswer === index) {
-			setCorrectAnswer(null); // Reset correct answer if it was deleted
+	useEffect(() => {
+		if (questionType) {
+			setCurrent(2); // questionType이 설정되면 다음 단계로 이동
 		}
+	}, [questionType]);
+
+	const questionTypes = [
+		'4개 중 맞는 문장 고르기',
+		'O / X 문제',
+		'4개 중 맞는 숫자 고르기',
+		'사용자 지정 유형'
+	];
+
+	const handleQuestionTypeSelect = type => {
+		setQuestionType(type);
+		setCurrent(2); // 다음 페이지로 이동
 	};
 
-	const handleToggleOption = index => {
-		if (correctAnswer === index) {
-			// If the same checkbox is clicked again, uncheck it
-			setCorrectAnswer(null);
-		} else {
-			// Otherwise, set this checkbox as the correct answer
-			setCorrectAnswer(index);
-		}
+	// const handleAddOption = () => {
+	// 	setOptions([...options, '']);
+	// };
+
+	// const handleOptionChange = (index, value) => {
+	// 	const newOptions = [...options];
+	// 	newOptions[index] = value;
+	// 	setOptions(newOptions);
+	// };
+
+	// const handleDeleteOption = index => {
+	// 	const newOptions = options.filter(
+	// 		(_, optionIndex) => optionIndex !== index
+	// 	);
+	// 	setOptions(newOptions);
+	// 	if (correctAnswer === index) {
+	// 		setCorrectAnswer(null); // Reset correct answer if it was deleted
+	// 	}
+	// };
+
+	// const handleToggleOption = index => {
+	// 	if (correctAnswer === index) {
+	// 		// If the same checkbox is clicked again, uncheck it
+	// 		setCorrectAnswer(null);
+	// 	} else {
+	// 		// Otherwise, set this checkbox as the correct answer
+	// 		setCorrectAnswer(index);
+	// 	}
+	// };
+	const handleSave = () => {
+		// 저장 로직 구현
+		console.log({questionType, question, options, correctAnswer});
+		onClose(); // 팝업 닫기
 	};
 
 	const handleSaveQuestion = async () => {
@@ -75,40 +101,58 @@ const QuizCreationOverlay = ({onClose, timestamp, src}) => {
 	};
 
 	return (
-		<Popup open={true} onClose={onClose} title="Create New Quiz Question">
-			<div>Current Timestamp: {timestamp}s</div>
-			<Input
-				placeholder="Enter your question here"
-				value={question}
-				onChange={e => setQuestion(e.value)}
-			/>
-			<Scroller direction="vertical" style={{maxHeight: '60vh'}}>
-				{options.map((option, index) => (
-					<div key={index} className="flex items-center my-2">
-						<Checkbox
-							onToggle={() => handleToggleOption(index)}
-							checked={correctAnswer === index}
-						/>
-						<Input
-							className="ml-2"
-							value={option}
-							onChange={e => handleOptionChange(index, e.value)}
-							size="small"
-							type="text"
-							placeholder="Enter option text"
-						/>
-						<Button
-							size="small"
-							icon="trash"
-							onClick={() => handleDeleteOption(index)}
-						></Button>
+		<Popup
+			open={true}
+			onClose={onClose}
+			title="Create New Quiz Question"
+			className="backdrop-blur-md"
+		>
+			<WizardPanels
+				current={0}
+				nextButtonVisibility="auto"
+				onBack={function noRefCheck() {}}
+				onChange={function noRefCheck() {}}
+				onNextClick={function noRefCheck() {}}
+				onPrevClick={function noRefCheck() {}}
+				onTransition={function noRefCheck() {}}
+				onWillTransition={function noRefCheck() {}}
+				prevButtonVisibility="auto"
+				prevButton={
+					<Button icon="closex" aria-label="Back">
+						Back
+					</Button>
+				}
+				nextButton={
+					<Button icon="closex" aria-label="Quit">
+						Close
+					</Button>
+				}
+			>
+				<Panel title="1. 문제의 종류를 선택하세요">
+					<div style={{display: 'flex', justifyContent: 'space-around'}}>
+						{['Type 1', 'Type 2', 'Type 3', 'Type 4'].map(type => (
+							<Button key={type} onClick={() => setQuestionType(type)}>
+								{type}
+							</Button>
+						))}
 					</div>
-				))}
-			</Scroller>
-			<Button icon="plus" onClick={handleAddOption}>
-				Add Option
-			</Button>
-			<Button onClick={handleSaveQuestion}>Add This Question</Button>
+				</Panel>
+				<Panel title="2. 문제를 입력하세요">
+					<Input value={question} onChange={e => setQuestion(e.value)} />
+				</Panel>
+				<Panel title="3. 선택지를 입력하세요">
+					{options.map((option, index) => (
+						<Input
+							key={index}
+							value={option}
+							onChange={e =>
+								setOptions(options.map((o, i) => (i === index ? e.value : o)))
+							}
+						/>
+					))}
+				</Panel>
+				<Panel title="4. 정답 선택">{/* 정답 선택 로직 */}</Panel>
+			</WizardPanels>
 		</Popup>
 	);
 };
