e86b41db9a7dd76f473d73190a8b9ec534221952
syh24 <dbsgur6896@naver.com>
2023-11-23 15:23:38 +0900

    ⚰️ 코드 정리

--- backend/routes/video.js
+++ backend/routes/video.js
@@ -30,32 +30,4 @@ router.post('/', async (req, res) => {
   }
 });
 
-// Update user
-// router.put('/:id', async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     const user = await User.findByIdAndUpdate(id, req.body, { new: true });
-//     if (!user) {
-//       return res.status(404).json({ message: 'User not found' });
-//     }
-//     res.json(user);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
-// // Delete user
-// router.delete('/:id', async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     const user = await User.findByIdAndDelete(id);
-//     if (!user) {
-//       return res.status(404).json({ message: 'User not found' });
-//     }
-//     res.sendStatus(204);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
 module.exports = router;
