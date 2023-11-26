5d3a9ed45c0e79161c563880e4e9a6001f9668b4
강상원 <kevink1113@u.sogang.ac.kr>
2023-11-26 12:36:07 +0900

    💄 Add color theme, update quiz creation overlay

--- frontend/src/views/QuizCreationOverlay.js
+++ frontend/src/views/QuizCreationOverlay.js
@@ -2,101 +2,101 @@ import React, {useState, useEffect} from 'react';
 import {WizardPanels, Slottable, Panel} from '@enact/sandstone/WizardPanels';
 import Button from '@enact/sandstone/Button';
 import Input from '@enact/sandstone/Input';
+import {InputField} from '@enact/sandstone/Input';
 import Checkbox from '@enact/sandstone/Checkbox';
 import Popup from '@enact/sandstone/Popup';
 import Scroller from '@enact/sandstone/Scroller';
 
-const QuizCreationOverlay = ({onClose, timestamp, src}) => {
-	const [current, setCurrent] = useState(0);
-	const [questionType, setQuestionType] = useState('');
+const QuizCreationOverlay = ({onClose}) => {
+	const [currentStep, setCurrentStep] = useState(0);
 	const [question, setQuestion] = useState('');
-	const [options, setOptions] = useState(['', '', '', '']);
-	const [correctAnswer, setCorrectAnswer] = useState(null);
+	const [options, setOptions] = useState(['', '', '', '']); // Assuming 4 options
+	const [questionType, setQuestionType] = useState('');
+	const totalSteps = 4; // Total number of steps
 
-	useEffect(() => {
-		if (questionType) {
-			setCurrent(2); // questionType이 설정되면 다음 단계로 이동
+	const nextStep = () => {
+		setCurrentStep(currentStep + 1);
+	};
+
+	const prevStep = () => {
+		if (currentStep > 0) {
+			setCurrentStep(currentStep - 1);
 		}
-	}, [questionType]);
-
-	const questionTypes = [
-		'4개 중 맞는 문장 고르기',
-		'O / X 문제',
-		'4개 중 맞는 숫자 고르기',
-		'사용자 지정 유형'
-	];
-
-	const handleQuestionTypeSelect = type => {
-		setQuestionType(type);
-		setCurrent(2); // 다음 페이지로 이동
 	};
 
-	// const handleAddOption = () => {
-	// 	setOptions([...options, '']);
-	// };
+	const StepIndicator = ({currentStep, totalSteps}) => {
+		return (
+			<div className="flex justify-center items-center mb-5">
+				{[...Array(totalSteps).keys()].map((step, index) => (
+					<React.Fragment key={step}>
+						{/* Circle */}
+						<div
+							className={`w-8 h-8 rounded-full flex items-center justify-center
+                                    ${
+																			currentStep === step
+																				? 'bg-bold text-white border-8 border-white'
+																				: 'bg-bold text-white'
+																		} mx-0`}
+						>
+							{step + 1}
+						</div>
 
-	// const handleOptionChange = (index, value) => {
-	// 	const newOptions = [...options];
-	// 	newOptions[index] = value;
-	// 	setOptions(newOptions);
-	// };
-
-	// const handleDeleteOption = index => {
-	// 	const newOptions = options.filter(
-	// 		(_, optionIndex) => optionIndex !== index
-	// 	);
-	// 	setOptions(newOptions);
-	// 	if (correctAnswer === index) {
-	// 		setCorrectAnswer(null); // Reset correct answer if it was deleted
-	// 	}
-	// };
-
-	// const handleToggleOption = index => {
-	// 	if (correctAnswer === index) {
-	// 		// If the same checkbox is clicked again, uncheck it
-	// 		setCorrectAnswer(null);
-	// 	} else {
-	// 		// Otherwise, set this checkbox as the correct answer
-	// 		setCorrectAnswer(index);
-	// 	}
-	// };
-	const handleSave = () => {
-		// 저장 로직 구현
-		console.log({questionType, question, options, correctAnswer});
-		onClose(); // 팝업 닫기
+						{/* Dotted Line Connector */}
+						{index < totalSteps - 1 && (
+							<div className="border-b-8 border-dotted border-bold w-16"></div>
+						)}
+					</React.Fragment>
+				))}
+			</div>
+		);
 	};
 
-	const handleSaveQuestion = async () => {
-		const problemText = options
-			.map((option, index) => `${index + 1}. ${option}`)
-			.join('\n');
-		const quizData = {
-			user_id: 3, // Replace with actual user ID
-			video_id: src, // Ensure 'src' is the video ID
-			problem: {
-				title: question,
-				problem: problemText
-			},
-			answer: correctAnswer + 1 // Adjust for 0-based index
-		};
-
-		try {
-			const response = await fetch('http://localhost:4000/api/quiz', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(quizData)
-			});
-
-			if (response.ok) {
-				console.log('Quiz added successfully');
-				onClose(); // Close the popup
-			} else {
-				console.error('Failed to add quiz');
-			}
-		} catch (error) {
-			console.error('Error:', error);
+	const renderStep = () => {
+		switch (currentStep) {
+			case 0:
+				return (
+					<>
+						<header>문제의 종류를 선택하세요. </header>
+						<div className="flex">
+							{[
+								'4개 중 맞는 문장 고르기',
+								'O X 문제',
+								'4개 중 맞는 숫자 고르기'
+							].map(type => (
+								<Button key={type} onClick={() => setQuestionType(type)}>
+									{type}
+								</Button>
+							))}
+						</div>
+					</>
+				);
+			case 1:
+				return (
+					<>
+						<header>문제를 입력하세요. </header>
+						<InputField
+							placeholder="문제를 입력하세요. (최대 50자)"
+							value={question}
+							onChange={e => setQuestion(e.value)}
+						/>
+					</>
+				);
+			case 2:
+				return options.map((option, index) => (
+					<InputField
+						key={index}
+						value={option}
+						onChange={e =>
+							setOptions(
+								options.map((o, i) => (i === index ? e.target.value : o))
+							)
+						}
+					/>
+				));
+			case 3:
+				return <div>{/* Answer selection logic */}</div>;
+			default:
+				return null;
 		}
 	};
 
@@ -105,54 +105,15 @@ const QuizCreationOverlay = ({onClose, timestamp, src}) => {
 			open={true}
 			onClose={onClose}
 			title="Create New Quiz Question"
-			className="backdrop-blur-md"
+			className="backdrop-blur-md rounded-xl h-[80vh] w-[93vw]"
 		>
-			<WizardPanels
-				current={0}
-				nextButtonVisibility="auto"
-				onBack={function noRefCheck() {}}
-				onChange={function noRefCheck() {}}
-				onNextClick={function noRefCheck() {}}
-				onPrevClick={function noRefCheck() {}}
-				onTransition={function noRefCheck() {}}
-				onWillTransition={function noRefCheck() {}}
-				prevButtonVisibility="auto"
-				prevButton={
-					<Button icon="closex" aria-label="Back">
-						Back
-					</Button>
-				}
-				nextButton={
-					<Button icon="closex" aria-label="Quit">
-						Close
-					</Button>
-				}
-			>
-				<Panel title="1. 문제의 종류를 선택하세요">
-					<div style={{display: 'flex', justifyContent: 'space-around'}}>
-						{['Type 1', 'Type 2', 'Type 3', 'Type 4'].map(type => (
-							<Button key={type} onClick={() => setQuestionType(type)}>
-								{type}
-							</Button>
-						))}
-					</div>
-				</Panel>
-				<Panel title="2. 문제를 입력하세요">
-					<Input value={question} onChange={e => setQuestion(e.value)} />
-				</Panel>
-				<Panel title="3. 선택지를 입력하세요">
-					{options.map((option, index) => (
-						<Input
-							key={index}
-							value={option}
-							onChange={e =>
-								setOptions(options.map((o, i) => (i === index ? e.value : o)))
-							}
-						/>
-					))}
-				</Panel>
-				<Panel title="4. 정답 선택">{/* 정답 선택 로직 */}</Panel>
-			</WizardPanels>
+			<StepIndicator currentStep={currentStep} totalSteps={totalSteps} />
+			{renderStep()}
+			<div>
+				{currentStep > 0 && <Button onClick={prevStep}>Back</Button>}
+				{currentStep < 3 && <Button onClick={nextStep}>Next</Button>}
+				{currentStep === 3 && <Button onClick={onClose}>Finish</Button>}
+			</div>
 		</Popup>
 	);
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				bold: '#F2B20F', // 강조색
+				primary: '#FFE3A3', // 보조색1 (아이콘)
+				secondary: '#FFF6E1', // 보조색2 (텍스트)
+				background: '#D5D5D5', // 배경색
+
+				'text-primary': '#595959' // 글꼴 (강조)
+			}
+		}
 	},
 	plugins: []
 };
