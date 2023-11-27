e1f7394978562f158addd7173222e4993d7b56f1
syh24 <dbsgur6896@naver.com>
2023-11-27 21:16:44 +0900

    🚑 hotfix

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var mysql = require('mysql');
 const path = require('path');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const passport = require('passport');
 const session = require('express-session');
@@ -9,62 +10,77 @@ const quizRouter = require('./routes/quiz');
 const videoRouter = require('./routes/video');
 const videoHistoryRouter = require('./routes/videoHistory');
 const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 //swagger docs
-var bodyParser = require('body-parser');
-var swaggerJsdoc = require('swagger-jsdoc');
-var swaggerUi = require('swagger-ui-express');
+var bodyParser = require("body-parser");
+var swaggerJsdoc = require("swagger-jsdoc");
+var swaggerUi = require("swagger-ui-express");
 
 passportConfig();
 dotenv.config();
 
 const app = express();
-
-app.use(cors()); // cors 사용
-
 const port = process.env.PORT || 4000;
 
-sequelize
-	.sync({ force: false })
-	.then(() => {
-		console.log('데이터베이스 연결 성공');
-	})
-	.catch((err) => {
-		console.error(err);
-	});
+app.use(cors());
+
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('데이터베이스 연결 성공');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: "vadvxcvcxvadsvasd",
+  cookie: {
+      httpOnly: true,
+      secure: false,
+  },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 //swagger
 const options = {
-	definition: {
-		openapi: '3.0.0',
-		info: {
-			title: 'Quiz App API',
-			version: '0.1.0',
-			description:
-				'This is a simple CRUD API application made with Express and documented with Swagger',
-			license: {
-				name: 'MIT',
-				url: 'https://spdx.org/licenses/MIT.html',
-			},
-			contact: {
-				name: 'test',
-				url: 'https://test.com',
-				email: 'info@email.com',
-			},
-		},
-		servers: [
-			{
-				url: 'http://localhost:4000',
-			},
-		],
-	},
-	apis: ['./swagger/user.js', './swagger/quiz.js', './swagger/video.js'],
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Quizkids API",
+        version: "0.1.0",
+        description:
+          "Quiz app",
+        contact: {
+          name: "서윤혁",
+          email: "dbsgur6896@naver.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:4000",
+        },
+      ],
+    },
+    apis: [
+        "./swagger/user.js",
+        "./swagger/quiz.js",
+        "./swagger/video.js",
+        "./swagger/videoHistory.js",
+    ],
 };
-
+  
 const specs = swaggerJsdoc(options);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+  app.use("/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+  );
 
 app.use('/api/users', userRouter);
 app.use('/api/quiz', quizRouter);
@@ -72,6 +88,9 @@ app.use('/api/videos', videoRouter);
 app.use('/api/videoHistories', videoHistoryRouter);
 
 //swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
