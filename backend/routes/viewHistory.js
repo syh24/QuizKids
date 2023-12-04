b8dd0e8848e0017ddd78ede5a9aa084a98ea5503
wandosan <ggyubbu@naver.com>
2023-12-04 16:25:14 +0900

    🚑 viewHistory의 response status 200으로 고정

--- backend/routes/viewHistory.js
+++ backend/routes/viewHistory.js
@@ -21,13 +21,13 @@ router.get('/', async (req, res) => {
         console.log(viewHistory[0])
 
         if (viewHistory[0] == undefined) {
-			return res.status(202).json({
+			return res.status(200).json({
 				result: 'fail',
 				message: '시청기록이 존재하지 않습니다.',
 			});
         }
         else {
-            return res.status(203).json({
+            return res.status(200).json({
 				result: 'success',
                 user_id: user_id,
                 video_id: video_id,
@@ -60,16 +60,14 @@ router.post('/', async (req, res) => {
         if (viewHistory[0] == undefined) {
             await newViewHistory.save();
             var massage = "시청기록 저장이 완료되었습니다.";
-            var status = 205;
         }
         else {
             viewHistory[0].stop_point = newViewHistory.stop_point;
             await viewHistory[0].save();
             var massage = "시청기록 업데이트가 완료되었습니다.";
-            var status = 206;
         }
 
-        return res.status(status).json({
+        return res.status(200).json({
             result: 'success',
             message: massage,
         });
