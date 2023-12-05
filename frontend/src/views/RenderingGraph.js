8bc283c8607dd8f83595860ca12298548987f1ed
Mengou <gjsehdrh@gmail.com>
2023-12-05 23:33:10 +0900

    🐛 change logic of showing system resource usage

--- frontend/src/views/RenderingGraph.js
+++ frontend/src/views/RenderingGraph.js
@@ -1,92 +1,73 @@
 import React from 'react';
 import {
-	Chart as ChartJS,
-	CategoryScale,
-	LinearScale,
-	BarElement,
-	Title,
-	Tooltip,
-	Legend
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement
 } from 'chart.js';
-import {Bar} from 'react-chartjs-2';
+import {Bar, Doughnut} from 'react-chartjs-2';
 // import {faker} from '@faker-js/faker';
 
 ChartJS.register(
-	CategoryScale,
-	LinearScale,
-	BarElement,
-	Title,
-	Tooltip,
-	Legend
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    ArcElement,
+    Title,
+    Tooltip,
+    Legend
 );
 
-const optionsForCpu = {
-	responsive: true,
-	scales: {
-		y: {
-			max: 100 // y축의 최대값을 100으로 설정
-		}
-	},
-	plugins: {
-		legend: {
-			position: 'top'
-		},
-		title: {
-			display: true
-			//text: 'System Resource Usage',
-		}
-	}
+const chartOptions = {
+    maintainAspectRatio: false,
+    responsive: true,
 };
 
-const optionsForMemory = {
-	responsive: true,
-	plugins: {
-		legend: {
-			position: 'top'
-		},
-		title: {
-			display: true
-		}
-	}
-};
-
-const labelsForCpu = ['Total Cpu', 'Cpu0', 'Cpu1', 'Cpu2', 'Cpu3'];
+const labelsForCpu = ['Total Cpu', 'Cpu 0', 'Cpu 1', 'Cpu 2', 'Cpu 3'];
+const cpuSegment = ['User', 'Nice', 'System', 'Idle'];
 const labelsForMemory = ['Available Memory', 'SwapUsed'];
 
-function RenderingGraph({cpuUsage, memoryUsage}) {
-	//console.log('usage', resourceUsage);
-	const dataForCpu = {
-		labels: labelsForCpu,
-		datasets: [
-			{
-				label: 'CpuUsage(%)',
-				data: labelsForCpu.map((elem, index) => cpuUsage[index]),
-				backgroundColor: 'rgba(255, 215, 0, 0.7)'
-			}
-		]
-	};
-
-	const data = {
-		labels: labelsForMemory,
-		datasets: [
-			{
-				label: 'Memory Usage(K)',
-				data: labelsForMemory.map((elem, index) => memoryUsage[index]),
-				backgroundColor: 'rgba(255, 127, 0, 0.7)'
-			}
-		]
-	};
-
-	return (
-		<div className="flex flex-col w-full h-full ml-10">
-			<div className="flex flex-col items-center justify-center sm:w-1/2 p-4 h-auto w-auto">
-				<Bar options={optionsForCpu} data={dataForCpu} />
+function RenderingGraph({cpuUsage}) {
+    //console.log('usage', resourceUsage);
+    return (
+		<div className="flex" style={{ height: '50vh', display: 'flex', flexWrap: 'wrap' }}>
+		  {cpuUsage.map((chartData, index) => (
+			<div
+			  key={index}
+			  style={{
+				width: index===0 ? '150px' : '100px',
+				height: index===0 ? '150px' : '100px',
+				margin: '10px',
+				flex: index === 0 ? '0 0 66%' : '0 0 33%',
+				marginTop: index===0 ? '50px' : '15px',
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center', // 가운데 정렬을 위해 추가
+				justifyContent: index === 0 ? 'center' : 'flex-start', // index가 0일 때만 화면 가운데 정렬
+			  }}
+			>
+			  <Doughnut
+				data={{
+				  labels: cpuSegment,
+				  datasets: [
+					{
+					  data: chartData,
+					  backgroundColor: ['yellow', 'blue', 'green', 'red'],
+					},
+				  ],
+				}}
+				options={chartOptions}
+			  />
+			  <h6 style={{ fontSize: '20px', fontWeight: 'bold' }}>{labelsForCpu[index]}</h6>
 			</div>
-			<div className="flex flex-col items-center justify-center sm:w-1/2 p-4 h-auto w-auto">
-				<Bar options={optionsForMemory} data={data} />
-			</div>
+		  ))}
 		</div>
-	);
-}
+	  );
+};
 
 export default RenderingGraph;
+
--- frontend/src/views/SystemState.js
+++ frontend/src/views/SystemState.js
@@ -6,139 +6,90 @@ import {getMemoryInfo, getCpuInfo} from '../libs/services';
 import RenderingGraph from './RenderingGraph';
 import BodyText from '@enact/sandstone/BodyText';
 import RenderingLoading from './RenderingLoading';
+import RenderingMemoryGraph from './RenderingMemoryGraph';
 
 const USER = 0;
 const NICE = 1;
 const SYSTEM = 2;
 const IDLE = 3;
 
-let usage = [];
+// let usage = [];
 
 const SystemState = () => {
-    const cpuRef = useRef(null);
-    const memRef = useRef(null);
-    
-    const [loading, setLoading] = useState(true); // 사용량 알 수 있을 때까지 1초 기다린 뒤, true로 set 됨
+	const cpuRef = useRef(null);
+	const memRef = useRef(null);
+	const [curCpu, setCurCpu] = useState([]);
+    const [curMem, setCurMem] = useState([]);
+	const [loading, setLoading] = useState(true); // 사용량 알 수 있을 때까지 1초 기다린 뒤, true로 set 됨
 
-    const [count, setCount] = useState(0);
+	const [cpuStat, setCpuStat] = useState({stat: [], returnValue: false});
+    const [memoryStat, setMemoryStat] = useState({returnValue: false});
 
-    const cpuStat = useRef({stat: [], returnValue: false});
-    const memStat = useRef({returnValue : false});
-    const prev = useRef([]);
-    const curr = useRef([]);
-    const saveCnt = useRef(0);
+	useEffect(() => {
+		if (!cpuRef.current) {
+			debugLog('GET_CONFIGS[R]', {});
+			cpuRef.current = getCpuInfo({
+				parameters: {
+					subscribe: true
+				},
+				onSuccess: res => {
+					debugLog('GET_CONFIGS[S]', res);
+					//cpuStat.current = res;
+					setCpuStat(res);
+				},
+				onFailure: err => {
+					debugLog('GET_CONFIGS[F]', err);
+				}
+			});
+		}
 
-    useEffect(() => {
-        // 설정된 시간 간격마다 setInterval 콜백이 실행된다. 
-        const id = setInterval(() => {
-            // 타이머 숫자가 하나씩 줄어들도록
-            setCount((current) => current + 1);
-            saveCnt.current = saveCnt.current+1;
+        if (!memRef.current) {
+			debugLog('GET_CONFIGS[R]', {});
+			memRef.current = getMemoryInfo({
+				parameters: {
+					subscribe: true
+				},
+				onSuccess: res => {
+					debugLog('GET_CONFIGS[S]', res);
+					//cpuStat.current = res;
+					setMemoryStat(res);
+				},
+				onFailure: err => {
+					debugLog('GET_CONFIGS[F]', err);
+				}
+			});
+		}
 
-            if (!cpuRef.current) {
-                //debugLog('GET_CONFIGS[R]', {});
-                cpuRef.current = getCpuInfo({
-                    parameters: {
-                        subscribe: true,
-                    },
-                    onSuccess: res => {
-                        //setCpuStat(res);
-                        //test_cpu = res;
-                        cpuStat.current = res;
-                    },
-                    onFailure: err => {
-                        debugLog('GET_CONFIGS[F]', err);
-                    }
-                });
-            }
+		let newCur = cpuStat.stat.slice(0, 5).map((element, index) => {
+			return element.split(/\s+/).slice(1, 5);
+		});
 
-            if (!memRef.current) {
-                //debugLog('GET_CONFIGS[R]', {});
-                memRef.current = getMemoryInfo({
-                    parameters: {
-                        subscribe: true,
-                    },
-                    onSuccess: res => {
-                        //setMemoryStat(res);
-                        //test_mem = res;
-                        memStat.current = res;
-                    },
-                    onFailure: err => {
-                        debugLog('GET_CONFIGS[F]', err);
-                    }
-                });
-            }
+		setCurCpu(newCur); // cur 상태 업데이트
+        setCurMem([memoryStat.usable_memory, memoryStat.swapUsed]);
 
-            console.log(saveCnt.current);
-            //console.log('cpu', cpuStat.current.stat);
-            //console.log('mem', memStat.current.returnValue);
-
-            //prev 계산
-            if(saveCnt.current === 1){
-                cpuStat.current.stat.slice(0,5).map((element, index) => {
-                    prev.current[index] = element.split(/\s+/).slice(1, 5);
-                }); 
-                console.log('prev', prev);
-            }
-
-            else{
-                usage = [];
-
-                cpuStat.current.stat.slice(0,5).map((element, index) => {
-                    curr.current[index] = element.split(/\s+/).slice(1, 5);
-                });
-
-                console.log('curr', curr);
-                
-                // 모든 cpu의 usage를 계산.
-                for(let i=0; i<curr.current.length; i++){
-                    const tmpCur = curr.current[i];
-                    const tmpPrev = prev.current[i];
-                    let Total = 0;
-                    let idle;
-                    //console.log(tmpCur, tmpPrev);
-                    //console.log(tmpCur[0]);
-                    for(let j=0; j<tmpCur.length; j++){
-                        let pad = tmpCur[j] - tmpPrev[j];
-                        Total += pad;
-                        if(j === IDLE) idle = pad;
-                    }
-
-                    //Total = 100;    // dummy
-
-                    idle = idle + 0.0; //+ Math.min(saveCnt.current, 99); // make idle floating point
-                    console.log('idle', idle);
-                    usage[i] = (1 - idle / Total) * 100;
-                    //console.log(`usage ${i}`, usage.current[i]);
-                }       
-
-                console.log('usage', usage);
-                if(saveCnt.current === 2) setLoading(false);
-                prev.current = curr.current; 
-            }
-        }, 1000);
-
-        return () => {
-            clearInterval(id); // 자원 낭비 방지
-            // check
-            if (cpuRef.current) {
-                cpuRef.current.cancel(); 
-                cpuRef.current = null;
-            }
+		return () => {
+			if (cpuRef.current) {
+				cpuRef.current.cancel();
+				cpuRef.current = null;
+			}
 
             if (memRef.current) {
-                memRef.current.cancel();
-                memRef.current = null;
-            }
-        }
-    }, [count]);
+				memRef.current.cancel();
+				memRef.current = null;
+			}
+		};
+	}, [cpuStat, memoryStat]);
 
-    //console.log(cpuStat);
-    return (
-        <div>
-            {loading ? <RenderingLoading /> : <RenderingGraph cpuUsage={usage} memoryUsage = {[memStat.current.usable_memory, memStat.current.swapUsed]}/>}
-        </div>
-    );
+	console.log(curCpu);
+    console.log(curMem);
+
+	return (
+		<div>
+			{/*loading ? <RenderingLoading /> : <RenderingGraph cpuUsage={usage} memoryUsage = {[memStat.current.usable_memory, memStat.current.swapUsed]}/>*/}
+			<RenderingGraph cpuUsage={curCpu} />
+            <RenderingMemoryGraph memoryUsage={curMem} />
+		</div>
+	);
 };
 
 export default SystemState;
